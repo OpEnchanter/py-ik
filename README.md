8257f11c91e576ba5a36143eca67dd666892181f
OpEnchanter <openchanter@gmail.com>
2025-01-14 10:03:21 -0500

    Fixed ik solving

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # py-ik
  
+This is a simple inverse kinematics solver I made in python.
+
+Inverse kinematics is a system of equations that solves for the angles of joints to move a set of linkages to a certain point while keeping the linkages always the same length. This is used often in robotics and with robots that have legs to give the script a position relative to the robot and the robot will attempt to move its leg to the specified point.
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame, sys, math
 
 window = pygame.display.set_mode((480, 480))
+pygame.display.set_caption("Pygame Inverse Kinematics")
 
 def calculateVector(p1, p2) -> tuple:
     return (p2[0] - p1[0], p2[1] - p1[1])
@@ -19,6 +20,8 @@ def calculateVertex(p1, p2, l1, l2) -> tuple:
     p1ToMidPoint = calculateDist(p1, midPoint)
 
     p1ToMid = calculateVector(p1, midPoint)
+    if magnitude(p1ToMid) < 1:
+        p1ToMid = (1, 1)
     p1ToMid = (p1ToMid[0] / magnitude(p1ToMid), p1ToMid[1] / magnitude(p1ToMid))
 
     newDist = p1ToMidPoint * l1 / l2
@@ -27,8 +30,6 @@ def calculateVertex(p1, p2, l1, l2) -> tuple:
         newDist = l1
 
     vertexBase = (p1[0] + p1ToMid[0] * newDist, p1[1] + p1ToMid[1] * newDist)
-
-    pygame.draw.circle(window, (255, 0, 0), vertexBase, 5)
 
     vertexBaseToP1 = calculateVector(vertexBase, p1)
     vertexBaseToP2 = calculateVector(vertexBase, p2)
@@ -41,8 +42,38 @@ def calculateVertex(p1, p2, l1, l2) -> tuple:
 
     return vertex
 
-length1 = 30
-length2 = 50
+def solveIk(p1, p2, l1, l2) -> tuple:
+    endPoint = p2
+
+    if calculateDist(endPoint, p1) > l1 + l2:
+        vec = calculateVector(p1, endPoint)
+        vec = (vec[0] / magnitude(vec), vec[1] / magnitude(vec))
+
+        endPoint = (p1[0] + vec[0] * (l1 + l2), p1[1] + vec[1] * (l1 + l2))
+
+    vertex = calculateVertex(p1, p2, l1, l2)
+
+
+    drawEnd = calculateVector(vertex, endPoint)
+
+    drawEnd = (drawEnd[0] / magnitude(drawEnd), drawEnd[1] / magnitude(drawEnd))
+
+    drawEnd = (vertex[0] + drawEnd[0] * l2, vertex[1] + drawEnd[1] * l2)
+
+    pygame.draw.line(window, (0,0,0), p1, vertex, 5)
+    pygame.draw.line(window, (0,0,0), vertex, drawEnd, 5)
+
+    pygame.draw.circle(window, (255, 0, 0), vertex, 5)
+    pygame.draw.circle(window, (0, 255, 0), p1, 5)
+    pygame.draw.circle(window, (0, 0, 255), drawEnd, 5)
+
+    return drawEnd
+
+length1 = 100
+length2 = 100
+
+target = pygame.mouse.get_pos()
+base = (240, 240)
 
 while True:
     for event in pygame.event.get():
@@ -52,33 +83,19 @@ while True:
 
     window.fill((255, 255, 255))
     
-    endPoint = pygame.mouse.get_pos()
+    target = (target[0] + calculateVector(target, pygame.mouse.get_pos())[0] / 1, target[1] + calculateVector(target, pygame.mouse.get_pos())[1] / 1)
+    
+    if pygame.key.get_pressed()[pygame.K_w]:
+        base = (base[0], base[1] - 0.1)
+    if pygame.key.get_pressed()[pygame.K_s]:
+        base = (base[0], base[1] + 0.1)
 
-    if calculateDist(endPoint, (240, 240)) > length1 + length2:
-        vec = calculateVector((240, 240), endPoint)
-        vec = (vec[0] / magnitude(vec), vec[1] / magnitude(vec))
+    if pygame.key.get_pressed()[pygame.K_a]:
+        base = (base[0] - 0.1, base[1])
+    if pygame.key.get_pressed()[pygame.K_d]:
+        base = (base[0] + 0.1, base[1])
 
-        endPoint = (240 + vec[0] * (length1 + length2), 240 + vec[1] * (length1 + length2))
+    firstEnd = solveIk(base, target, 50, 50)
 
-    vertex = calculateVertex((240, 240), pygame.mouse.get_pos(), length1, length2)
-
-
-    drawEnd = calculateVector(vertex, endPoint)
-    f = length2 / length1
-
-    totalDist = magnitude(drawEnd) * f
-
-    print(magnitude(drawEnd))
-
-    drawEnd = (drawEnd[0] / magnitude(drawEnd), drawEnd[1] / magnitude(drawEnd))
-
-    drawEnd = (240 + drawEnd[0] * totalDist, 240 + drawEnd[1] * totalDist)
-
-    pygame.draw.circle(window, (255, 0, 0), vertex, 5)
-    pygame.draw.circle(window, (255, 0, 0), (240, 240), 5)
-    pygame.draw.circle(window, (255, 0, 0), endPoint, 5)
-
-    pygame.draw.line(window, (0,0,0), (240, 240), vertex, 5)
-    pygame.draw.line(window, (0,0,0), vertex, drawEnd, 5)
 
     pygame.display.flip()
